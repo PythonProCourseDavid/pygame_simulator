b9f4367a64e3f840feacbb6c87b65a1c4c59a2e3
DenisHrtn <denispython16@gmail.com>
2024-07-02 17:36:53 +0300

    all components in simulation module

--- simulation/population.py
+++ simulation/population.py
@@ -12,8 +12,36 @@ class Population:
         self.city = city
         self.people = [Person(width, height, city) for _ in range(NUMS_OF_PERSONS)]
         self.infected_random_people()
+        self.peak_incubating = 0
 
     def infected_random_people(self):
         random_people = random.choice(self.people)
         random_people.infect()
 
+    def get_infected_count(self):
+        return sum(1 for person in self.people if person.is_infected())
+
+    def get_incubation_count(self):
+        return sum(1 for person in self.people if person.is_incubating())
+
+    def get_health_count(self):
+        return sum(1 for person in self.people if person.health_status == 'healthy')
+
+    def update(self):
+        incubating_count = self.get_incubation_count()
+        infected_count = self.get_infected_count()
+
+        if incubating_count > self.peak_incubating:
+            self.peak_incubating = incubating_count
+
+        for person in self.people:
+            person.move()
+            for other_person in self.people:
+                if person != other_person and person.rect.colliderect(other_person):
+                    person.contact_with(other_person)
+                    other_person.contact_with(person)
+            person.update_health_status()
+
+    def draw(self, screen):
+        for person in self.people:
+            person.draw(screen)
